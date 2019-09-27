9e6fe522378eeb233f9b5cf835af3e841c67764d
Indranil Gayen <nil.gayen@gmail.com>
2019-09-27 16:24:45 +0530

    Dev Cycle for Next Version - introduced shafts (v3) - [test] - Other shafts required to be created - working on compressed files - introduce grammer - todo %<^% kind of symbols for NNE etc - No merge to stable before next version

--- R/detect_and_read.R
+++ R/detect_and_read.R
@@ -4,6 +4,7 @@
 #' @include file_etc.R
 #' @include detect_and_read_shafts.R
 
+
 # detect and read file type (and potentially read) based on content type
 detect_and_read <- function(fn, silent = FALSE, omit = NULL) {
   common_file_error(fn, silent = silent)
