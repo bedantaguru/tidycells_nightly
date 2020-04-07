ec2c98bf25b8f19a408ed09da6ffef12193b7bfb
Indranil Gayen <nil.gayen@gmail.com>
2020-04-07 11:51:28 +0530

    Nightly Rearrange

--- 00_nightly_only/dev/example_tuning.R
+++ 00_nightly_only/dev/example_tuning.R
@@ -4,7 +4,7 @@ options(lifecycle_verbosity = "warning")
 #  it has to be added as test
 
 #non uniformity  among different reader
-tf0 <- read_cells("00_nightly_only/example_tuning_check_points/master_pattern.xlsx")[[1]]
+tf0 <- read_cells("00_nightly_only/dev/example_tuning_check_points/master_pattern.xlsx")[[1]]
 tf0 <- tf0 %>% detect_table_block()
 tf0 %>% filter(Row = 1, Col = 1, keep= 5) -> xx
 # xx %>% mutate(gid_29 = gid_29+gid_13, gid_13 = gid_14+gid_13) %>% plot()
@@ -20,6 +20,9 @@ d1 <- collate_columns(d0)
 # name suggest will come here
 name_suggest(d1, ca)
 
+
+
+d <- readxl::read_excel(f, sheet = "Table_1.12", col_names = F, col_types = "text")
 
 
 # 
